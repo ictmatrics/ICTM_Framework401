aa3171aee26a33edb7da75e26d72b609bb6ee2a9
ICT Matrics <166489933+ictmatrics@users.noreply.github.com>
2026-06-24 12:50:25 +0545

    Add files via upload
    patch minor error

--- APP/Config/Constants.php
+++ APP/Config/Constants.php
@@ -13,11 +13,19 @@ $protocol = (
     || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) === 'https')
 ) ? 'https://' : 'http://';
 
+// Get the directory and normalize slashes
 $scriptDir = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'] ?? '')), '/');
-$host      = $_SERVER['HTTP_HOST'] ?? 'localhost';
-$baseUrl   = $protocol . $host ;
-define('BASE_URL', $baseUrl);
 
+// FIX 1: Strip "public_html" from the internal path so it doesn't leak into your redirects
+$scriptDir = preg_replace('#/public_html$#', '', $scriptDir);
+
+$host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+$baseUrl = $protocol . $host . $scriptDir;
+
+// FIX 2: Standardize the trailing slash
+$baseUrl = rtrim($baseUrl, '/') . '/';
+
+define('BASE_URL', $baseUrl);
 
 //Mailer setting
   define('E_HOST', 'mail.domain.com');
